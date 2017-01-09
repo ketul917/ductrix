e53be09555d2601e4bfa6780781e13a9bab47412
Kevin Patel <ketul917@gmail.com>
2017-01-09 22:10:53 +0000

    config.py: changed the root of ductrix

--- web/config.py
+++ web/config.py
@@ -1,4 +1,4 @@
 SQLALCHEMY_DATABASE_URI = "postgresql://ductdb:Randm109@dbhost/postgres"
 SQLALCHEMY_DATABASE_SCHEMA = "db1"
-DUCTRIX_ROOT = '/var/hfiles/'
+DUCTRIX_ROOT = '/var/ductrix/src/'
 GRAFANA_PORT = '3000'
