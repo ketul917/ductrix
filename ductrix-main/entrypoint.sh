f4f21043cecc9c298b0c2dc9423e1bac194fcad4
Kevin Patel <ketul917@gmail.com>
2017-01-13 19:32:56 -0500

    checkpoint

--- ductrix-main/entrypoint.sh
+++ ductrix-main/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cd /var/ductrix ; git pull
+supervisord --config /var/ductrix/web/supervisord.conf
+
 if [ "$1" == "-d" ]; then
 	echo "Running app in debug mode!"
 	python /var/ductrix/web/app.py
